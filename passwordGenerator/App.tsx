c95704b2d19177990a23f263a55ce19b39f0a531
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2023-03-06 15:46:27 +0530

    pass-gen creating logic

--- passwordGenerator/App.tsx
+++ passwordGenerator/App.tsx
@@ -23,7 +23,12 @@ export default function App() {
   };
 
   const createPassword = (characters: string, passwordLength: number) => {
-    //
+    let result = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
   };
 
   return (
